6ca5a98b379e010987a642f6afa121a9084cbe29
Ecy <yefeng@hotmail.fr>
2025-03-24 03:13:03 +0900

    wip

--- extensions/cocostudio/loader/load.js
+++ extensions/cocostudio/loader/load.js
@@ -278,3 +278,387 @@ cc.loader.register(["json"], {
         });
     }
 });
+
+ccs._generateAttribute = function(output, input) {
+    output["Name"] = input.getName();
+
+    const color = input.getColor() || cc.color(0, 0, 0, 0);
+    output["CColor"] = {
+        r: color.r,
+        g: color.g,
+        b: color.b,
+        a: color.a,
+    };
+
+    output["Position"] = {
+        X: input.getPositionX(),
+        Y: input.getPositionY(),
+    };
+
+    output["Scale"] = {
+        ScaleX: input.getScaleX(),
+        ScaleY: input.getScaleY(),
+    };
+
+    output["RotationSkewX"] = input.getRotationX();
+    output["RotationSkewY"] = input.getRotationY();
+
+    const anchor = input.getAnchorPoint();
+    output["AnchorPoint"] = {
+        ScaleX: anchor.x,
+        ScaleY: anchor.y,
+    };
+
+    output["ZOrder"] = input.getLocalZOrder();
+    output["VisibleForFrame"] = input.isVisible();
+
+    const size = input.getContentSize();
+    output["Size"] = {
+        X: size.width,
+        Y: size.height,
+    };
+    output["Alpha"] = input.getOpacity();
+    output["Tag"] = input.getTag();
+    // output["ActionTag"] = ; // TODO: find a solution for this, it is very likely auto generated from an INT32
+    // output["UserData"] = ;
+    output["FrameEvent"] = "";
+
+    ccs._generateLayoutComponent(output, input);
+}
+
+ccs._generateLayoutComponent = function(output, input) {
+    // Logic copied from setLayoutComponent implementation
+    const layout = ccui.LayoutComponent.bindLayoutComponent(input);
+    if (!layout)
+        return;
+
+    output["PrePosition"] = { X: 0, Y: 0 };    
+    if(layout.isPositionPercentXEnabled()) {
+        output["PositionPercentXEnabled"] = true;
+        output["PrePosition"]["X"] = layerComponent.getPositionPercentX();
+    }
+
+    if(layout.isPositionPercentYEnabled()) {
+        output["PositionPercentYEnabled"] = true;
+        output["PrePosition"]["Y"] = layerComponent.getPositionPercentY();
+    }
+
+    output["PreSize"] = { X: 0, Y: 0 };
+    if(layout.isPercentWidthEnabled()) {
+        output["PercentWidthEnable"] = true;
+        output["PreSize"]["X"] = layerComponent.getPercentWidth();
+    }
+
+    if(layout.isPercentHeightEnabled()) {
+        output["PercentHeightEnable"] = true;
+        output["PreSize"]["Y"] = layerComponent.getPercentHeight();
+    }
+
+    if(layout.isStretchWidthEnabled()) {
+        output["StretchWidthEnable"] = true;
+    }
+
+    if(layout.isStretchHeightEnabled()) {
+        output["StretchHeightEnable"] = true;
+    }
+
+    const horizontalEdge = layout.getHorizontalEdge();
+    switch(horizontalEdge) {
+        case ccui.LayoutComponent.horizontalEdge.LEFT: {
+            output["HorizontalEdge"] = "LeftEdge";
+        }break;
+
+        case ccui.LayoutComponent.horizontalEdge.RIGHT: {
+            output["HorizontalEdge"] = "RightEdge";
+        }break;
+
+        case ccui.LayoutComponent.horizontalEdge.CENTER: {
+            output["HorizontalEdge"] = "BothEdge";
+        }break;
+
+        case ccui.LayoutComponent.horizontalEdge.NONE:
+        default: break;
+    }
+
+    const verticalEdge = layout.getVerticalEdge();
+    switch(verticalEdge) {
+        case ccui.LayoutComponent.verticalEdge.TOP: {
+            output["VerticalEdge"] = "TopEdge";
+        }break;
+
+        case ccui.LayoutComponent.verticalEdge.BOTTOM: {
+            output["VerticalEdge"] = "BottomEdge";
+        }break;
+
+        case ccui.LayoutComponent.verticalEdge.CENTER: {
+            output["VerticalEdge"] = "BothEdge";
+        }break;
+
+        case ccui.LayoutComponent.verticalEdge.NONE:
+        default: break;
+    }
+    
+    output["LeftMargin"] = layout.getLeftMargin();
+    output["RightMargin"] = layout.getRightMargin();
+    output["TopMargin"] = layout.getTopMargin();
+    output["BottomMargin"] = layout.getBottomMargin();
+}
+
+ccs._generateWidgetAttributes = function(output, input) {
+    
+} 
+
+ccs._serializers = {
+    // SingleNodeObjectData for root ObjectData node
+    // NodeObjectData for nodes inside ObjectData
+    // LayerObjectData if has auto layout 
+    // Not used: GameNodeObjectData, GameLayerObjectData
+    node: function(node) {
+        const outputNode = {};
+        ccs._generateAttribute(outputNode, node);
+        outputNode["ctype"] = "SingleNodeObjectData";
+
+        return outputNode;
+    },
+
+    scene: function(node) {
+        const outputNode = {};
+        ccs._generateAttribute(outputNode, node);
+        outputNode["ctype"] = "SingleNodeObjectData";
+        outputNode["Name"] = "Scene";
+
+        return outputNode;
+    },
+
+    layer: function(node) {
+        const outputNode = {};
+        ccs._generateAttribute(outputNode, node);
+        outputNode["ctype"] = "LayerObjectData";
+
+        return outputNode;
+    },
+
+    // SpriteObjectData: cc.Sprite
+    sprite: function(node) {
+        const outputNode = {};
+        ccs._generateAttribute(outputNode, node);
+        outputNode["ctype"] = "SpriteObjectData";
+
+        const blend = node.getBlendFunc();
+        if(blend) {
+            outputNode["BlendFunc"] = {
+                "Src": blend.src,
+                "Dst": blend.dst,
+            };
+        }
+
+        if(node.isFlippedX()) {
+            output["FlipX"] = true;
+        }
+
+        if(node.isFlippedY()) {
+            output["FlipY"] = true;
+        }
+
+        const tex = node.getTexture();
+        if(tex) {
+            // TODO: implement
+        }
+
+        return outputNode;
+    },
+
+    // ParticleObjectData: cc.ParticleSystem
+    particle: function(node) {
+    },
+
+    // PanelObjectData: ccui.Layout
+    panel: function(node) {
+
+    },
+    
+    // TextObjectData: ccui.Text
+    text: function(node) {
+        
+    },
+
+    // ButtonObjectData: ccui.Button
+    button: function(node) {
+        
+    },
+
+    // CheckBoxObjectData: ccui.CheckBox
+    checkBox: function(node) {
+        
+    },
+
+    // ScrollViewObjectData: ccui.ScrollView
+    scrollView: function(node) {
+
+    },
+
+    // ImageViewObjectData: ccui.ImageView
+    imageView: function(node) {
+
+    },
+
+    // LoadingBarObjectData: ccui.LoadingBar
+    loadingBar: function(node) {
+
+    },
+
+    // SliderObjectData: ccui.Slider
+    slider: function(node) {
+          
+    },
+
+    // PageViewObjectData: ccui.PageView
+    pageView: function(node) {
+
+    },
+    
+    // ListViewObjectData: ccui.ListView
+    listView: function(node) {
+        
+    },
+
+    // TextAtlasObjectData: ccui.TextAtlas
+    textAtlas: function(node) {
+    },
+    
+    // TextBMFontObjectData: ccui.TextBMFont
+    textBMF: function(node) {
+    },
+
+    // TextFieldObjectData: ccui.TextField
+    textField: function(node) {
+    },
+
+    // SimpleAudioObjectData: ccs.ComAudio
+    simpleObject: function(node) {
+    },
+
+    // GameMapObjectData: could be node or cc.TMXTiledMap
+    gameMap: function(node) {
+    },
+
+    // ProjectNodeObjectData: file data, could be anything
+    projectNode: function(node) {
+    },
+
+    // ArmatureNodeObjectData: ccs.Armature
+    armature: function(node) {
+    },
+
+    // BoneNodeObjectData: ccs.Armature
+    bone: function(data) {
+    },
+
+    // SkeletonNodeObjectData: ccs.Armature
+    skeleton: function(node) {
+    },
+}
+
+ccs._nodeTypeTable = {
+    "Scene": ccs._serializers.scene,
+    "Node": ccs._serializers.node,
+    "Layer": ccs._serializers.layer,
+    "Sprite": ccs._serializers.sprite,
+    "ParticleSystem": null,
+
+    "Widget": null,
+    "Layout": null,
+    "Text": null,
+    "Button": null,
+    "CheckBox": null,
+    "ScrollView": null,
+    "ImageView": null,
+    "LoadingBar": null,
+    "Slider": null,
+    "PageView": null,
+    "ListView": null,
+    "TextAtlas": null,
+    "TextBMFont": null,
+    "TextFieldTTF": null,
+    "TMXTiledMap": null,
+    
+    // NOTE: not sure
+    "LabelTTF": null,
+    "Menu": null,
+    "MenuItem": null,
+    "Scale9Sprite": null,
+    "ScrollViewBar": null,
+    "ClippingNode": null,
+    "AtlasNode": null,
+    "PageViewIndicator": null,
+    
+    // NOTE: not implemented
+    "LayerColor": null,
+    "LayerGradient": null,
+    "LayerMultiplex": null,
+    "LabelAtlas": null,
+    "LabelBMFont": null,
+    "MotionStreak": null,
+    "Armature": null,
+    "Bone": null,
+    "Skin": null,
+};
+
+// NOTE: returns a node
+ccs._serializeNode = function(node) {
+    // Assert parent defined
+    if(!node) {
+        return;
+    }
+
+    if(!node._className) {
+        console.warn("Node type not found:", node);
+    }
+
+    const serializer = ccs._nodeTypeTable[node._className];
+    if(!serializer) {
+        console.warn("Serializer not found for type:", node.className);
+        return;
+    }
+
+    const outputNode = serializer(node);
+    if(!outputNode) {
+        console.warn("Failed to serialize node.", node);
+    }
+
+    outputNode.children = [];
+
+    for(let index = 0; 
+        index < node.children.length;
+        ++index)
+    {
+        const generatedNode = ccs._serializeNode(node.children[index]);
+        if(generatedNode) {
+            outputNode.children.push(generatedNode);
+        }
+    }
+
+    return outputNode;
+}
+
+ccs.export = function(root) {
+    let output = {};
+
+    output["ID"] = crypto.randomUUID(); // generate UUID
+    output["Version"] = "2.0.0.0"; // Version of CocosStudio, but it also affects parsing
+    output["Name"] = "MainScene";
+    output["Type"] = "Scene"; // Could be something else?
+    output["Content"] = {};
+    output["Content"]["Content"] = {
+        "ObjectData": {},
+        "Animation": {},
+        "AnimationList": [],
+        "ctype": "GameProjectData" // See which one works better, GameNodeObjectData or GameLayerObjectData
+    };
+    
+    if(!root) return;
+
+    output["Content"]["Content"]["ObjectData"] = ccs._serializeNode(root);
+
+    return output;
+}
